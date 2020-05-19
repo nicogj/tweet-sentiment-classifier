df41b5ec29cb1de2ea916de5dee0f7a1c4dd4af2
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2020-05-19 20:02:02 +0200

    initial commit

---
+++ scripts/build_classifier.py
@@ -0,0 +1,17 @@
+from simpletransformers.classification import ClassificationModel
+import pandas as pd
+import logging
+
+# Train and Evaluation data needs to be in a Pandas Dataframe of two columns. The first column is the text with type str, and the second column is the label with type int.
+train_df = pd.read_csv('data/train.tsv', sep='\t', encoding='latin')
+
+test_df = pd.read_csv('data/test.tsv', sep='\t', encoding='latin')
+
+# Create a ClassificationModel
+model = ClassificationModel('bert', 'bert-base-cased', use_cuda=False, args={'overwrite_output_dir': True})
+
+# Train the model
+model.train_model(train_df, output_dir = 'models')
+
+# Evaluate the model
+result, model_outputs, wrong_predictions = model.eval_model(test, verbose=True)
