cd043cf59ec43659cdf027e0c35d050d90acf4cf
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2020-05-19 21:12:23 +0200

    added optional train/test split value

--- scripts/build_classifier.py
+++ scripts/build_classifier.py
@@ -1,14 +1,24 @@
+# python3 scripts/build_classifier.py --train_size 0.50
+
 from simpletransformers.classification import ClassificationModel
 import pandas as pd
-import logging
+import argparse
 
-# Train and Evaluation data needs to be in a Pandas Dataframe of two columns. The first column is the text with type str, and the second column is the label with type int.
-train_df = pd.read_csv('data/train.tsv', sep='\t', encoding='latin')
+from utils import split_train_test
 
-test_df = pd.read_csv('data/test.tsv', sep='\t', encoding='latin')
+if __name__ == '__main__':
 
-# Create a ClassificationModel
-model = ClassificationModel('bert', 'bert-base-cased', use_cuda=False, args={'overwrite_output_dir': True})
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_size', default=0.75, type=float,
+                        help='What is the size of the training set?')
+    args = parser.parse_args()
 
-# Train the model
-model.train_model(train_df, output_dir = 'models')
+    # Generate Training set
+    split_train_test(train_size = args.train_size)
+    train_df = pd.read_csv('data/train.tsv', sep='\t', encoding='latin')
+
+    # Create a ClassificationModel
+    model = ClassificationModel('bert', 'bert-base-cased', use_cuda=False, args={'overwrite_output_dir': True})
+
+    # Train the model
+    model.train_model(train_df, output_dir = 'models')
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,17 +1,16 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-TEST_SIZE = 0.25
 random_state = 123
 
-if __name__ == '__main__':
-    # Read in data
-    df = pd.read_csv('data/training.1600000.processed.noemoticon.csv', encoding='latin', header=None)
+def split_train_test(train_size):
+
+    df = pd.read_csv('data/training_1600000_processed_noemoticon.csv', encoding='latin', header=None)
     df = df[[0,5]]
     df.columns = ['labels', 'text']
     df['labels'] = [0 if x==0 else 1 for x in df['labels']]
 
-    df_train, df_test = train_test_split(df, test_size=TEST_SIZE, random_state=random_state)
+    df_train, df_test = train_test_split(df, test_size=1-train_size, random_state=random_state)
 
     print("TRAIN size:", df_train.shape[0])
     print("TEST size:", df_test.shape[0])
